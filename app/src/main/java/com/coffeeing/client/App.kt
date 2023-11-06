a3fd7635fcfe9d8999223b3917e4d605fb44e509
jihyunniiii <jita1125@naver.com>
2023-11-06 21:45:00 +0900

    [init] #1 확장 함수 추가

--- app/src/main/java/com/coffeeing/client/App.kt
+++ app/src/main/java/com/coffeeing/client/App.kt
@@ -1,11 +1,14 @@
 package com.coffeeing.client
 
 import android.app.Application
+import com.coffeeing.client.util.CoffeeingDebugTree
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
+        if (BuildConfig.DEBUG) Timber.plant(CoffeeingDebugTree())
     }
 }
