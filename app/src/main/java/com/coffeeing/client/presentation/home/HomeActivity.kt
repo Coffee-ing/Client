bd38537fc29d45a390ae7b275e4dfa60887c67d2
jihyunniiii <jita1125@naver.com>
2023-11-07 17:28:14 +0900

    [feat] #3 홈 뷰 구현

---
+++ app/src/main/java/com/coffeeing/client/presentation/home/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.coffeeing.client.presentation.home
+
+import android.os.Bundle
+import com.coffeeing.client.R
+import com.coffeeing.client.databinding.ActivityHomeBinding
+import com.coffeeing.client.util.binding.BindingActivity
+
+class HomeActivity : BindingActivity<ActivityHomeBinding>(R.layout.activity_home) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
