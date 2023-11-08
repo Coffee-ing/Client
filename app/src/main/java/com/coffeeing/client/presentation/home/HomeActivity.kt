12b3186cfac3fd35020ad9d3e43533dda8315c9c
jihyunniiii <jita1125@naver.com>
2023-11-08 16:31:41 +0900

    [feat] #3 커핑 리사이클러뷰 구현 (더미 데이터 추가)

--- app/src/main/java/com/coffeeing/client/presentation/home/HomeActivity.kt
+++ app/src/main/java/com/coffeeing/client/presentation/home/HomeActivity.kt
@@ -1,12 +1,30 @@
 package com.coffeeing.client.presentation.home
 
 import android.os.Bundle
+import android.view.View
+import androidx.activity.viewModels
 import com.coffeeing.client.R
 import com.coffeeing.client.databinding.ActivityHomeBinding
 import com.coffeeing.client.util.binding.BindingActivity
 
 class HomeActivity : BindingActivity<ActivityHomeBinding>(R.layout.activity_home) {
+    private val viewModel: HomeViewModel by viewModels()
+    lateinit var homeCoffeeingAdapter: HomeCoffeeingAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        binding.viewModel = viewModel
+        initLayout()
+    }
+
+    private fun initLayout() {
+        homeCoffeeingAdapter = HomeCoffeeingAdapter()
+        binding.rvHomeCoffeeing.adapter = homeCoffeeingAdapter
+        homeCoffeeingAdapter.submitList(viewModel.mockHomeCoffeeingList)
+
+        if (viewModel.mockHomeCoffeeingList.isEmpty()) {
+            binding.rvHomeCoffeeing.visibility = View.INVISIBLE
+            binding.layoutHomeEmpty.visibility = View.VISIBLE
+        }
     }
 }
