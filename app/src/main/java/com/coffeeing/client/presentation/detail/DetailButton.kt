028f1edbdc0326c47686e4f826e7dcf81192260a
jihyunniiii <jita1125@naver.com>
2023-11-10 03:10:38 +0900

    [del] #11 불필요한 파일 삭제

--- app/src/main/java/com/coffeeing/client/presentation/detail/DetailButton.kt
+++
@@ -1,9 +0,0 @@
-package com.coffeeing.client.presentation.detail
-
-import androidx.annotation.DrawableRes
-
-data class DetailButton(
-    @DrawableRes
-    val button_num : Int, //이게 없으면 오류남
-    val button_name: String,
-)
